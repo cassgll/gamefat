916fd2b336e7cd3c463ab93a95de5cb7c253de7c
Casio <cassgabriel@outlook.com>
2023-05-01 18:47:44 -0300

    zipping the project's .css and .js files

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,1 @@
-const button = document.getElementById("button");
-const nav = document.getElementById("nav");
-
-button.addEventListener("click", () => {
-	nav.classList.toggle("active");
-});
-
-const navLink = document.querySelectorAll(".nav-link");
-
-navLink.forEach((element) => {
-	element.addEventListener("click", () => {
-		nav.classList.remove("active");
-	});
-});
+const button=document.getElementById("button"),nav=document.getElementById("nav");button.addEventListener("click",()=>{nav.classList.toggle("active")});const navLink=document.querySelectorAll(".nav-link");navLink.forEach(t=>{t.addEventListener("click",()=>{nav.classList.remove("active")})});
