da29ec9e3ff7d7b96fd6e279ecaf1a4cf43d1665
Casio <cassgabriel@outlook.com>
2023-05-02 06:38:45 -0300

    Changing the JavaScript format

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,11 @@
-const button=document.getElementById("button"),nav=document.getElementById("nav");button.addEventListener("click",()=>{nav.classList.toggle("active")});const navLink=document.querySelectorAll(".nav-link");navLink.forEach(t=>{t.addEventListener("click",()=>{nav.classList.remove("active")})});
+const button = document.getElementById("button"),
+	nav = document.getElementById("nav");
+button.addEventListener("click", () => {
+	nav.classList.toggle("active");
+});
+const navLink = document.querySelectorAll(".nav-link");
+navLink.forEach((t) => {
+	t.addEventListener("click", () => {
+		nav.classList.remove("active");
+	});
+});
