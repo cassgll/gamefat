c54be4640623ed3b67e97cefd5e758829536938d
Casio <cassgabriel@outlook.com>
2023-05-05 09:48:03 -0300

    Adding scroll animation and class changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,35 @@
+// Menu Mobile
 const button = document.getElementById("button"),
 	nav = document.getElementById("nav");
+
 button.addEventListener("click", () => {
 	nav.classList.toggle("active");
 });
 const navLink = document.querySelectorAll(".nav-link");
+
 navLink.forEach((t) => {
 	t.addEventListener("click", () => {
 		nav.classList.remove("active");
 	});
+});
+
+// Animation Scroll
+const allElements = document.querySelectorAll(
+	"[wm-animation]"
+);
+
+function animeScroll() {
+	const windowTop =
+		window.pageYOffset + (window.innerHeight * 3) / 4;
+	allElements.forEach((e) => {
+		if (windowTop >= e.offsetTop) {
+			e.classList.add("animating");
+		} else {
+			e.classList.remove("animating");
+		}
+	});
+}
+
+window.addEventListener("scroll", () => {
+	animeScroll();
 });
